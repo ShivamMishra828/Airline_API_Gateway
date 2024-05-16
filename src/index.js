32bc6f80e92175a00c13807d2a6a23824f9500d7
ShivamMishra828 <shivam9878522087@gmail.com>
2024-05-16 20:21:53 +0530

    FEAT: Added Rate-limiter and also added Reverse Proxy via http-proxy-middleware package

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@ const express = require("express");
 const morgan = require("morgan");
 const apiRoutes = require("./routes");
 const { ServerConfig, LoggerConfig } = require("./config");
+const rateLimit = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const PORT = ServerConfig.PORT;
 const app = express();
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     morgan("myFormat", {
         stream: LoggerConfig.accessLogStream,
+    })
+);
+app.use(limiter);
+app.use(
+    "/flightsService",
+    createProxyMiddleware({
+        target: ServerConfig.FLIGHT_SERVICE,
+        changeOrigin: true,
+    })
+);
+app.use(
+    "/bookingService",
+    createProxyMiddleware({
+        target: ServerConfig.BOOKING_SERVICE,
+        changeOrigin: true,
     })
 );
 app.use("/api", apiRoutes);
